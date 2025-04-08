06acfbd5a06e20f1e55e72bc49248e85a1a206b3
aviennx <avienn.98@outlook.com>
2025-04-08 19:37:19 +0530

    Simplify main.cpp to only handle PPM output

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,23 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>
-#include <vector>
-
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "../third_party/stb_image_write.h"
-
-void write_ppm(int width, int height, const std::vector<unsigned char>& pixels) {
-    // PPM Header
-    std::cout << "P3\n" << width << ' ' << height << "\n255\n";
-
-    for (int j = 0; j < height; j++) {
-        std::clog << "\rScanlines remaining: " << (height - j) << ' ' << std::flush;
-        for (int i = 0; i < width; i++) {
-            int index = (j * width + i) * 3;
-            std::cout << static_cast<int>(pixels[index]) << ' '
-                      << static_cast<int>(pixels[index + 1]) << ' '
-                      << static_cast<int>(pixels[index + 2]) << '\n';
-        }
-    }
-    std::clog << "\rDone.                 \n";
-}
-
-void write_png(const char* filename, int width, int height, const std::vector<unsigned char>& pixels) {
-    stbi_write_png(filename, width, height, 3, pixels.data(), width * 3);
-}
 
 int main() {
-    // Image dimensions
-    const int image_width = 20;
-    const int image_height = 20;
+    // Image
+    int image_width = 256;
+    int image_height = 256;
 
-    // Create pixel buffer
-    std::vector<unsigned char> pixels(image_width * image_height * 3);
+    // Render
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-    // Generate gradient
     for (int j = 0; j < image_height; j++) {
-        std::clog << "\rGenerating pixels: " << (image_height - j) << " scanlines remaining " << std::flush;
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0.0;
-
-            int index = (j * image_width + i) * 3;
-            pixels[index] = static_cast<unsigned char>(255.999 * r);
-            pixels[index + 1] = static_cast<unsigned char>(255.999 * g);
-            pixels[index + 2] = static_cast<unsigned char>(255.999 * b);
+            auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
+            write_color(std::cout, pixel_color);
         }
     }
-    std::clog << "\rDone generating pixels.                 \n";
 
-    // Write PPM to stdout
-    write_ppm(image_width, image_height, pixels);
-
-    // Write PNG to file
-    std::clog << "Writing PNG file...\n";
-    write_png("image.png", image_width, image_height, pixels);
-    std::clog << "Done writing PNG file.\n";
-
-    return 0;
+    std::clog << "\rDone.                 \n";
 } 
